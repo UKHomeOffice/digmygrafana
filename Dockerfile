403586affb6e688c5d1780310c5cd971bbf3697e
Ben Marvell <madmarv187@gmail.com>
2020-09-29 23:44:57 +0100

    update grafana to 6.6

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM grafana/grafana:6.4.4
+FROM grafana/grafana:6.6.2
 USER root
 RUN apk update \
  && apk add jq \
--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl https://download.sysdig.com/stable/grafana-sysdig-datasource/grafana-sysdig-datasource-v0.6.tgz -o sysdig.tgz
+curl https://download.sysdig.com/stable/grafana-sysdig-datasource/grafana-sysdig-datasource-v0.7.tgz -o sysdig.tgz
 
 tar zxf sysdig.tgz -C .
 
